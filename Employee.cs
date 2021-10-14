881b72b04a433e316e449daf5f55e4c8a15989da
anamikasawhney <anamika.sawhney22@gmail.com>
2021-10-14 10:10:11 +0530

    Uploaded

---
+++ Employee.cs
@@ -0,0 +1,73 @@
+﻿ using System;
+  class Employee
+    {
+        readonly int id;
+        string name;
+        static string dept;
+        int salary;
+        public const string companyName="UST" ;
+        // Default Constructor
+          public Employee() {} 
+        // Parameterized Constructor
+        public Employee(string name)
+        {
+            this.name = name;
+            Console.WriteLine("Enter ID");
+            id= Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Enter Department");
+            dept = Console.ReadLine();
+            Console.WriteLine("Enter Salary");
+            salary = Convert.ToInt16(Console.ReadLine());
+        }
+        // Parameterized Constructor
+        public Employee(int id, string name)
+        {
+            this.name = name;
+             
+            this.id= id;
+            Console.WriteLine("Enter Department");
+            dept = Console.ReadLine();
+            Console.WriteLine("Enter Salary");
+            salary = Convert.ToInt16(Console.ReadLine());
+        }
+        // Fully Parameterized Constructor
+        public Employee(int id, string name, string dept, int salary)
+        {
+            this.name = name;
+             
+            this.id= id;
+             
+            this.dept = dept;
+            
+            this.salary = salary;
+            }
+   // Static Constructor , Parameterless
+            static Employee()
+            {
+                dept="HR";
+            }
+        public  void GetDetails()
+        {
+            Console.WriteLine("Enter ID");
+            id= Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            name = Console.ReadLine();
+// Console.WriteLine("Enter Department");
+// dept = Console.ReadLine();
+Console.WriteLine("Enter Salary");
+salary = Convert.ToInt16(Console.ReadLine());
+}
+public void DisplayDetails()
+ {
+Console.WriteLine("ID is " + id);
+Console.WriteLine("Name is " + name);
+// Console.WriteLine("Department is " + dept);
+Console.WriteLine("Salary is " + salary);
+ }
+ public static void DepartmentDetails()
+ {
+   dept="HR";
+   // name="";
+     Console.WriteLine("Dept is " +  dept);
+ }
+  }
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EmployeeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Employee employee1 = new Employee();
+          employee1.GetDetails();
+          employee1.DisplayDetails();
+
+          Employee employee2 = new  Employee("Raakesh");
+        //   employee2.GetDetails();
+          employee2.DisplayDetails();
+
+          Employee employee3 = new  Employee(10, "Athira");
+        //   employee3.GetDetails();
+          employee3.DisplayDetails();
+          Employee employee4 = new  Employee(11, "Neeraj","IT",25000);
+          employee4.DisplayDetails();
+
+        }
+    }
+}
